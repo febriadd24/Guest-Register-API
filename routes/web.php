30cc4509d65283ee782c83f8a6f2a7e1b5648372
febriadd24 <febriadd24@gmail.com>
2019-07-04 18:16:28 +0700

    change dir to/bukutamu

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/bukutamu', function () {
     return view('app');
 });
 Route::resource('/daftartamu','DaftartamuController',['except' => ['show']]);
